8cfb26616ce455776ab16b7ef7d4d7d9ca1f319a
unknown <atiquefabiha@gmail.com>
2023-11-15 01:49:02 -0600

    adding widgts

---
+++ exploring_widgets/lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(0, 175, 18, 57),
+        
+        body: Center(
+          child: Text('Fabiha Atique '),
+        ),
+      ),
+    ),
+  );
+}
